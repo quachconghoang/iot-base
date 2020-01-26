20a410722c5e3da78da34c2a0544509e419005b8
hoangqc <hoangqc@vnu.edu.vn>
2020-01-26 16:34:10 +0700

    NVDecode on OpenCV done!

---
+++ README.md
@@ -0,0 +1,6 @@
+FFMPEG configuration:
+--enable-cuda-nvcc --enable-cuvid --enable-nvenc --enable-nonfree --enable-libnpp --enable-pic --enable-shared --extra-cflags=-I/usr/local/cuda/include --extra-ldflags=-L/usr/local/cuda/lib64
+
+OPENCV build with CUDA CODEC:
+...
+
--- Test/nvidia-optflow/main.cpp
+++ Test/nvidia-optflow/main.cpp
@@ -1,40 +1,42 @@
 #include "opencv2/opencv.hpp"
-
+cv::cudacodec::VideoReader * nvCap;
 
 int main()
 {
-    std::string ip = "192.168.100.13";
+    cv::cuda::setDevice(0);
     cv::VideoCapture vCap;
     cv::Mat image;
 
-    std::string command = "rtsp://admin:123@Abc.@192.168.100.13:554/Streaming/Channels/101/";
+    std::string command = "rtsp://admin:123@Abc.@192.168.1.136:554/Streaming/Channels/101/";
+//    command = "/home/hoangqc/star_wars_1080p.mp4";
 
-    //    cv::cuda::GpuMat img1, img2;
-//    cv::cudacodec::VideoReader * nvCap = cv::cudacodec::createVideoReader(command);
-//    while(true)
-//    {
-//        nvCap->nextFrame(img1);
-//        img1.download(image);
-//        cv::resize(image,image,cv::Size(640,360));
-//        cv::imshow("Output Window", image);
-//        if(cv::waitKey(1) >= 0) break;
-//    }
+    cv::Ptr<cv::cudacodec::VideoReader> nvCap = cv::cudacodec::createVideoReader(command);
 
-    vCap.open(command);
+    cv::cuda::GpuMat img1, img2;
 
-    const std::string n = vCap.getBackendName();
-    while (true)
+    while(true)
     {
-        if(!vCap.read(image)) {
-            std::cout << "No frame" << std::endl;
-            cv::waitKey();
-        }
-        cv::resize(image,image,cv::Size(640,360));
+        nvCap->nextFrame(img1);
+        img1.download(image);
+        cv::resize(image,image,cv::Size(960,540));
         cv::imshow("Output Window", image);
-
         if(cv::waitKey(1) >= 0) break;
     }
-    vCap.release();
+
+//    vCap.open(command);
+//    const std::string n = vCap.getBackendName();
+//    while (true)
+//    {
+//        if(!vCap.read(image)) {
+//            std::cout << "No frame" << std::endl;
+//            cv::waitKey();
+//        }
+//        cv::resize(image,image,cv::Size(640,360));
+//        cv::imshow("Output Window", image);
+//
+//        if(cv::waitKey(1) >= 0) break;
+//    }
+//    vCap.release();
     cv::destroyAllWindows();
     return 0;
 }
