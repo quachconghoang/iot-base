6e3f157baf3d24b0e526a8ba41ce2a2db4f7cf8e
hoangqc <hoangqc@vnu.edu.vn>
2020-01-26 17:34:18 +0700

    Fix crash Code (delay 30ms needed)

--- Test/nvidia-optflow/main.cpp
+++ Test/nvidia-optflow/main.cpp
@@ -6,37 +6,39 @@ int main()
     cv::cuda::setDevice(0);
     cv::VideoCapture vCap;
     cv::Mat image;
+    cv::cuda::GpuMat img1, img2;
 
     std::string command = "rtsp://admin:123@Abc.@192.168.1.136:554/Streaming/Channels/101/";
 //    command = "/home/hoangqc/star_wars_1080p.mp4";
 
     cv::Ptr<cv::cudacodec::VideoReader> nvCap = cv::cudacodec::createVideoReader(command);
-
-    cv::cuda::GpuMat img1, img2;
+//    vCap.open(command);
 
     while(true)
     {
-        nvCap->nextFrame(img1);
-        img1.download(image);
-        cv::resize(image,image,cv::Size(960,540));
-        cv::imshow("Output Window", image);
-        if(cv::waitKey(1) >= 0) break;
-    }
-
-//    vCap.open(command);
-//    const std::string n = vCap.getBackendName();
-//    while (true)
-//    {
-//        if(!vCap.read(image)) {
-//            std::cout << "No frame" << std::endl;
-//            cv::waitKey();
+        if(!nvCap->nextFrame(img1)) {
+            std::cout << "No GPU frame" << std::endl;
+            if(cv::waitKey(30) >= 0) break;
+        }else{
+            img1.download(image);
+            cv::resize(image,image,cv::Size(960,540));
+            cv::imshow("GPU Output Window", image);
+            if(cv::waitKey(30) >= 0) break;
+        }
+//        if(!vCap.read(image)){
+//            std::cout << "No CPU frame" << std::endl;
+//            cv::waitKey(30);
 //        }
-//        cv::resize(image,image,cv::Size(640,360));
-//        cv::imshow("Output Window", image);
-//
-//        if(cv::waitKey(1) >= 0) break;
-//    }
+//        else{
+//            cv::resize(image,image,cv::Size(960,540));
+//            cv::imshow("CPU Output Window", image);
+//            if(cv::waitKey(1) >= 0) break;
+//        }
+
+    }
 //    vCap.release();
+    nvCap.release();
+
     cv::destroyAllWindows();
     return 0;
 }
