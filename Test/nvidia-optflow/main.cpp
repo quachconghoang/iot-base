60dd501c4f305704bda13dcabe638e4536cc3c5d
hoangqc <hoangqc@vnu.edu.vn>
2020-02-01 09:55:54 +0700

    GPU Decode with config file

--- Test/nvidia-optflow/main.cpp
+++ Test/nvidia-optflow/main.cpp
@@ -2,19 +2,22 @@
 
 int main()
 {
+//SETUP CONFIGS
+    std::string filePath = std::string(getenv("HOME")) + "/IP_Camera.json";
+    cv::FileStorage fs(filePath, 0);
+    std::string vProtocol = fs["protocol"];
+    std::string vIP = fs["locations"][0];
+    std::string vUsrName = fs["user-id"];
+    std::string vUsrPsw = fs["user-pwd"];
+    std::string vInput = vProtocol + vUsrName + ":" + vUsrPsw + "@" + vIP;
+
     cv::cuda::setDevice(0);
     cv::VideoCapture vCap;
     cv::Mat image;
     cv::cuda::GpuMat img1, img2;
+    cv::Ptr<cv::cudacodec::VideoReader> nvCap = cv::cudacodec::createVideoReader(vInput);
 
-    std::string command = "rtsp://admin:123@Abc.@192.168.1.136:554/Streaming/Channels/101/";
-//    command = "/home/hoangqc/star_wars_1080p.mp4";
-
-    cv::Ptr<cv::cudacodec::VideoReader> nvCap = cv::cudacodec::createVideoReader(command);
-//    vCap.open(command);
-
-    while(true)
-    {
+    while(true) {
         if(!nvCap->nextFrame(img1)) {
             std::cout << "No GPU frame" << std::endl;
             if(cv::waitKey(30) >= 0) break;
@@ -24,20 +27,8 @@ int main()
             cv::imshow("GPU Output Window", image);
             if(cv::waitKey(30) >= 0) break;
         }
-//        if(!vCap.read(image)){
-//            std::cout << "No CPU frame" << std::endl;
-//            cv::waitKey(30);
-//        }
-//        else{
-//            cv::resize(image,image,cv::Size(960,540));
-//            cv::imshow("CPU Output Window", image);
-//            if(cv::waitKey(1) >= 0) break;
-//        }
-
     }
-//    vCap.release();
     nvCap.release();
-
     cv::destroyAllWindows();
     return 0;
 }
