8470b0426c468f710bcecb8a4990efdad5e1452e
hoangqc <hoangqc@vnu.edu.vn>
2020-02-10 01:46:25 +0700

    mosquitto mqtt

---
+++ Test/mosquitto/temperature_conversion.h
@@ -0,0 +1,20 @@
+#ifndef TEMPERATURE_CONVERSION_H
+#define TEMPERATURE_CONVERSION_H
+
+#include <mosquittopp.h>
+#include <string>
+
+class mqtt_tempconv : public mosqpp::mosquittopp
+{
+public:
+    mqtt_tempconv(const char *id, const char *host, int port);
+    ~mqtt_tempconv();
+
+    std::string topicName = "test";
+
+    void on_connect(int rc);
+    void on_message(const struct mosquitto_message *message);
+    void on_subscribe(int mid, int qos_count, const int *granted_qos);
+};
+
+#endif
