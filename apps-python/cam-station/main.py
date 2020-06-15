739d6331457bdf9dc79312b83a7de3833e35210f
quachconghoang <mr_doraemon_meomeo@yahoo.com>
2020-06-15 09:37:34 +0700

    app icons

--- apps-python/cam-station/main.py
+++ apps-python/cam-station/main.py
@@ -2,10 +2,12 @@ import sys
 from PyQt5.QtWidgets import QApplication
 from mainwindow import MainWindow
 from PyQt5.Qt import PYQT_VERSION_STR
+from PyQt5.QtGui import QIcon
 
 if __name__ == "__main__":
     print("PyQt version:", PYQT_VERSION_STR)
     app = QApplication(sys.argv)
+    app.setWindowIcon(QIcon("./Resources/fire.png"))
 
     window = MainWindow()
     window.show()
