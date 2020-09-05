0cd04fe6c01ce0ec23ea35d89a9af6262c692a65
QuachCongHoang <mr_doraemon_meomeo@yahoo.com>
2020-09-05 18:17:47 +0700

    hardwork

--- apps-python/cam-station/testImage.py
+++ apps-python/cam-station/testImage.py
@@ -8,5 +8,11 @@ next = cv2.imread("/home/hoangqc/Desktop/Frames-05/frame1155.jpg")
 next = cv2.cvtColor(next, cv2.COLOR_BGR2GRAY)
 flow = cv2.calcOpticalFlowFarneback(prvs, next, None, 0.5, 3, 15, 3, 5, 1.2, 0)
 
-plt.imshow(flow)
-plt.show()
+flow_x = flow[..., 0]
+flow_y = flow[..., 1]
+ret1, th_x = cv2.threshold(flow_x, 0.8, 1, cv2.THRESH_BINARY)
+cv2.imshow('th_x', th_x)
+
+cv2.imshow('flow_x', flow_x)
+cv2.imshow('flow_y', flow_y)
+cv2.waitKey()
--- apps-python/cam-station/testVideo.py
+++ apps-python/cam-station/testVideo.py
@@ -1,13 +1,16 @@
 import numpy as np
 import cv2
 
-path4 = "/home/hoangqc/Desktop/005.mp4"
+path4 = "/home/hoangqc/Desktop/006.mp4"
+raw_size = (1280, 720)
+proc_size = (640, 360)
+proc_roi = (140, 0, 360, 360)
 
 cap = cv2.VideoCapture(path4)
 count = 0
 
 ret, dumb = cap.read()
-frame = cv2.resize(dumb, dsize=(640, 360))
+frame = cv2.resize(dumb, proc_size)
 prvs = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
 
 
@@ -21,20 +24,46 @@ while(True):
     ret = cap.grab()
     if (ret):
 
-        if(count%15 == 0):
+        if(count%5 == 0):
             cap.read(dumb)
-            cv2.imwrite('/home/hoangqc/Desktop/Frames-05/frame'+str(count)+'.jpg',dumb)
-            frame = cv2.resize(dumb, dsize=(640, 360))
-            cv2.imshow('frame', frame)
+            # cv2.imwrite('/home/hoangqc/Desktop/Frames-05/frame'+str(count)+'.jpg',dumb)
+            frame = cv2.resize(dumb, proc_size)
             next = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
             flow = cv2.calcOpticalFlowFarneback(prvs, next, None, 0.5, 3, 15, 3, 5, 1.2, 0)
-            mag, ang = cv2.cartToPolar(flow[..., 0], flow[..., 1])
-            hsv[..., 0] = ang * 180 / np.pi / 2
-            hsv[..., 2] = cv2.normalize(mag, None, 0, 255, cv2.NORM_MINMAX)
-            rgb = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
-            cv2.imshow('frame2', rgb)
+
+            flow_func = 0.8*abs(flow[..., 1]) + 0.2*abs(flow[..., 0])
+            minVal, maxVal, minLoc, maxLoc = cv2.minMaxLoc(flow_func)
+            # print(maxVal, maxLoc)
+
+            ret1, flow_mask = cv2.threshold(flow_func, 0.8, 1, cv2.THRESH_BINARY)
+            points = np.nonzero(flow_mask)
+            point_org = (proc_size[0]/2,proc_size[1]/2)
+
+            num_point = points[0].size
+            print(num_point)
+
+            if num_point == 0:
+                point_org = (10,10)
+            else:
+                point_org = (np.mean(points[1]).astype(int),
+                             np.mean(points[0]).astype(int))
+            print(point_org)
+
+            # cv2.imshow('flow_mask', flow_mask)
+            # cv2.imshow('flow_x', flow_x)
+            # cv2.imshow('flow_y', flow_y)
+
+            if num_point>150:
+                cv2.circle(frame, maxLoc, 7, (255,255,0), 2)
+                cv2.circle(frame, point_org, 7, (0,0,255), 2)
+
+            cv2.rectangle(frame, (140,0), (140+360,360), (0,0,255), 1)
+
+            cv2.imshow('frame', frame)
 
             prvs = next
+            # cv2.waitKey()
+
 
         count += 1
     # Our operations on the frame come here
@@ -43,7 +72,7 @@ while(True):
     # Display the resulting frame
     # cv2.imshow('frame',frame)
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    if cv2.waitKey(10) & 0xFF == ord('q'):
         break
 
 # When everything done, release the capture
