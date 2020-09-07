3fbf787fcdf88b96a315cc32afaa57f9c2970860
quachconghoang <mr_doraemon_meomeo@yahoo.com>
2020-09-07 10:01:24 +0700

    update size process

--- apps-python/cam-station/testVideo.py
+++ apps-python/cam-station/testVideo.py
@@ -4,15 +4,26 @@ import cv2
 path4 = "/home/hoangqc/Desktop/006.mp4"
 raw_size = (1280, 720)
 proc_size = (640, 360)
-proc_roi = (140, 0, 360, 360)
+
+p0 = (250, 0)
+roi_size = (360, 360)
+p1 = (p0[0]+roi_size[0],p0[1]+roi_size[1])
+
+proc_roi = (p0[0], p0[1], roi_size[0], roi_size[1])
+
+flow_points_thresh = 1000
+
+dl_size = (300,300)
+
+
 
 cap = cv2.VideoCapture(path4)
 count = 0
 
 ret, dumb = cap.read()
 frame = cv2.resize(dumb, proc_size)
-prvs = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-
+# prvs = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+prvs = np.zeros(roi_size,dtype=np.uint8)
 
 hsv = np.zeros_like(frame)
 hsv[...,1] = 255
@@ -24,53 +35,51 @@ while(True):
     ret = cap.grab()
     if (ret):
 
-        if(count%5 == 0):
+        if(count%15 == 0):
             cap.read(dumb)
             # cv2.imwrite('/home/hoangqc/Desktop/Frames-05/frame'+str(count)+'.jpg',dumb)
             frame = cv2.resize(dumb, proc_size)
-            next = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            roi_img = frame[p0[1]:p1[1], p0[0]:p1[0]]
+            next = cv2.cvtColor(roi_img, cv2.COLOR_BGR2GRAY)
             flow = cv2.calcOpticalFlowFarneback(prvs, next, None, 0.5, 3, 15, 3, 5, 1.2, 0)
 
             flow_func = 0.8*abs(flow[..., 1]) + 0.2*abs(flow[..., 0])
-            minVal, maxVal, minLoc, maxLoc = cv2.minMaxLoc(flow_func)
+            # minVal, maxVal, minLoc, maxLoc = cv2.minMaxLoc(flow_func)
             # print(maxVal, maxLoc)
 
             ret1, flow_mask = cv2.threshold(flow_func, 0.8, 1, cv2.THRESH_BINARY)
+
             points = np.nonzero(flow_mask)
-            point_org = (proc_size[0]/2,proc_size[1]/2)
+            point_org = (p1[0]/2, p1[1]/2)
 
             num_point = points[0].size
-            print(num_point)
+
 
             if num_point == 0:
-                point_org = (10,10)
+                pass
             else:
                 point_org = (np.mean(points[1]).astype(int),
                              np.mean(points[0]).astype(int))
             print(point_org)
 
-            # cv2.imshow('flow_mask', flow_mask)
+            cv2.imshow('flow_mask', flow_mask)
             # cv2.imshow('flow_x', flow_x)
             # cv2.imshow('flow_y', flow_y)
 
-            if num_point>150:
-                cv2.circle(frame, maxLoc, 7, (255,255,0), 2)
-                cv2.circle(frame, point_org, 7, (0,0,255), 2)
+            if num_point>flow_points_thresh:
+                # cv2.circle(roi_img, maxLoc, 7, (255,255,0), 2)
+                print(num_point)
+                cv2.circle(roi_img, point_org, 7, (0,0,255), 2)
 
-            cv2.rectangle(frame, (140,0), (140+360,360), (0,0,255), 1)
+            cv2.rectangle(frame, p0, p1, (0,0,255), 1)
 
             cv2.imshow('frame', frame)
+            # cv2.imshow('roi',roi_img)
 
             prvs = next
             # cv2.waitKey()
 
-
         count += 1
-    # Our operations on the frame come here
-    # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-    # Display the resulting frame
-    # cv2.imshow('frame',frame)
 
     if cv2.waitKey(10) & 0xFF == ord('q'):
         break
